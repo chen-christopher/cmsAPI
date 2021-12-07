79e6df09d8805d005d3604737034f434acf859e4
Chris <pcc386@nyu.edu>
2021-12-06 22:02:08 -0500

    styling

--- script.js
+++ script.js
@@ -7,41 +7,6 @@ const app = {
     });
     console.log("hello");
   },
-  // getEntry: (entry) => {
-  //   console.log("HERE13231");
-  //   // a known issue with the contentful library is that embedded images are ignored in rich text
-  //   // this is the current workaround: https://github.com/contentful/rich-text/issues/61
-  //   // const options = {
-  //   //   renderNode: {
-  //   //     "embedded-asset-block": ({
-  //   //       data: {
-  //   //         target: { fields },
-  //   //       },
-  //   //     }) => {
-  //   //       // debugger;
-  //   //       return `<img src="${fields.file.url}" height="${fields.file.details.image.height}" width="${fields.file.details.image.width}" alt="${fields.description}"/>`;
-  //   //     },
-  //   //   },
-  //   // };
-  //   app.client.getEntry(entry).then((project) => {
-  //     debugger;
-  //     const projectData = {
-  //       animalName: project.fields.animalName,
-  //       imageUrl: `http:${project.fields.image.fields.file.url}`,
-  //       imageTitle: project.fields.image.fields.title,
-  //     };
-  //     console.log(projectData);
-  //     // load the template for this item from a local file
-  //     // fetch("projectPage.mustache")
-  //     //   .then((response) => response.text())
-  //     //   .then((template) => {
-  //     //     // render the template with the data
-  //     //     const rendered = Mustache.render(template, projectData);
-  //     //     // add the element to the container
-  //     //     $(".container").append(rendered);
-  //     //   });
-  //   });
-  // },
   getAllEntries: async () => {
     // first make sure we have our template loaded
     // i can use the word await along with async to pause the program until this function is finished
@@ -60,22 +25,12 @@ const app = {
           lastName: project.fields.lastName,
           imageUrl: `http:${project.fields.image.fields.file.url}`,
           imageTitle: project.fields.image.fields.title,
+          imageDescription: project.fields.image.fields.description,
         };
         console.log(projectData);
         const rendered = Mustache.render(template, projectData);
         // add the element to the container
         $(".container").append(rendered);
-        // debugger;
-        // const projectData = {
-        //   title: project.fields.title,
-        //   imageUrl: `http:${project.fields.image.fields.file.url}`,
-        //   imageTitle: project.fields.image.fields.title,
-        //   slug: `${project.fields.slug}.html`,
-        // };
-        // console.
-        // const rendered = Mustache.render(template, projectData);
-        // // add the element to the container
-        // $(".container").append(rendered);
       });
     });
   },
